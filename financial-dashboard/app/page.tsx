fe9e54a2c6b47ff57f640a166ca859edc8db80fc
Javid Rezai <javid.af@outlook.com>
2023-12-29 02:44:17 +0100

    Update homepage information

--- financial-dashboard/app/page.tsx
+++ financial-dashboard/app/page.tsx
@@ -15,11 +15,10 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Financial Dashboard built with Nextjs 14.</strong> This is a practice example for Next.js 14 from the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
           </p>
           <Link
             href="/login"
